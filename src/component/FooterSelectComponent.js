ddc9943e3fade74155d4c7aec874d74082d5dd5e
HYKTSNG <Sin5hayak@gmail.com>
2023-04-30 16:42:09 +0900

    文末のselect文を別ファイルへ移動

---
+++ src/component/FooterSelectComponent.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function FooterSelectComponent({ selectedOption, handleSelectChange }) {
+  return (
+    <div>
+      <label htmlFor="select-options">Choose a closing statement:　</label>
+      <select
+        id="select-options"
+        value={selectedOption}
+        onChange={handleSelectChange}
+      >
+        <option value="">---</option>
+        <option value="箇条書きで教えてください">箇条書き</option>
+        <option value="200文字以内で教えてください">200文字以内</option>
+        <option value="簡潔に教えてください">簡潔に</option>
+        <option value="小学生にもわかるように教えてください">
+          小学生にもわかるように
+        </option>
+        <option value="ステップ形式で教えてください">ステップ形式</option>
+      </select>
+    </div>
+  );
+}
+
+export default FooterSelectComponent;

baf6981cd839a74e83a5e4a88fbbd28fa7b061df
HYKTSNG <Sin5hayak@gmail.com>
2023-04-30 16:15:02 +0900

    文末に加える文をselect形式で追加

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -1,35 +1,64 @@
 import React, { useState } from "react";
 
 function TextForm() {
-  const [text, setText] = useState("");
+  const [inputText, setInputText] = useState("");
 
-  const handleChange = (event) => {
-    setText(event.target.value);
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(inputText);
+  };
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
   };
 
   return (
     <div>
-      <div>先頭に追加する文字: </div>
-      <div>あなたはプログラマーです</div>
+      <div>
+        <label htmlFor="input-text">Enter some text:</label>
+      </div>
       <textarea
+        id="input-text"
         type="text"
-        onChange={handleChange}
-        value={text}
-        style={styles.inputLarge}
+        value={inputText}
+        onChange={handleInputChange}
       />
-      <p>
-        あなたはプログラマーです。 <br />
-        {text} <br />
-        小学生にもわかるように教えてください
-      </p>
+
+      <div>
+        <label htmlFor="select-options">Choose a closing statement: </label>
+        <select
+          id="select-options"
+          value={selectedOption}
+          onChange={handleSelectChange}
+        >
+          <option value="">---</option>
+          <option value="箇条書きで教えてください">箇条書き</option>
+          <option value="200文字以内で教えてください">200文字以内</option>
+          <option value="簡潔に教えてください">簡潔に</option>
+          <option value="小学生にもわかるように教えてください">
+            小学生にもわかるように
+          </option>
+          <option value="ステップ形式で教えてください">ステップ形式</option>
+        </select>
+      </div>
+
+      {inputText && (
+        <div>
+          <p>
+            あなたはプログラマーです。 <br />
+            {inputText}
+            {selectedOption && <p>{selectedOption}</p>}
+          </p>
+          <button onClick={handleCopyClick}>Copy</button>
+        </div>
+      )}
     </div>
   );
 }
-
-const styles = {
-  inputLarge: {
-    width: "50%",
-  },
-};
 
 export default TextForm;
